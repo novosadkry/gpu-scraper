43936e37d6369e81d1698a2126246119e7f3d3f1
Kryštof Novosad <novosadkry@seznam.cz>
2021-05-16 09:56:44 +0200

    Store products

--- main.py
+++ main.py
@@ -2,13 +2,52 @@ from CZC import CZC
 from Alza import Alza
 from product import Product
 
-import json
+import jsons
 
-products = {}
+storedProducts = {}
+
+def onProductUpdated(product: Product):
+    print(jsons.dumps(storedProducts[product.name]))
+
+
+def addNewProduct(product: Product):
+    storedProducts[product.name] = {
+        product.store: {
+            "price": product.price,
+            "inStock": product.inStock
+        }
+    }
+
+
+def updateProduct(product: Product):
+    updated = False
+
+    if product.store not in storedProducts[product.name]:
+        updated = True
+    else:
+        if storedProducts[product.name][product.store]["price"] != product.price:
+            updated = True
+        if storedProducts[product.name][product.store]["inStock"] != product.inStock:
+            updated = True
+
+    storedProducts[product.name][product.store] = {
+        "price": product.price,
+        "inStock": product.inStock
+    }
+
+    if updated:
+        onProductUpdated(product)
+
 
 def onProductFetch(product: Product):
-    print(product.name, product.price, product.inStock, sep=', ')
-    products[product.name] = product
+    if product.inStock == 0:
+        return
+
+    if product.name in storedProducts:
+        updateProduct(product)
+    else:
+        addNewProduct(product)
+
 
 Alza().scrape(onProductFetch)
 CZC().scrape(onProductFetch)
