57d6df3e04bbd646201a46e93bc16bb7b37d3576
Kryštof Novosad <novosadkry@seznam.cz>
2021-05-17 13:49:48 +0200

    Update only if changed

--- product.py
+++ product.py
@@ -13,19 +13,32 @@ class Product():
         self.inStock = inStock
         self.link = link
 
+    def compare(self, other):
+        if self.price != other.price: return False
+        if self.inStock != other.inStock: return False
+        return True
+
+storedProducts = {}
 redis = Redis(host = 'ip.zahrajto.wtf', port = 25543, password = 'tvojemama')
 fetchLock = threading.Lock()
 
+def hasProductChanged(product: Product):
+    if product.id in storedProducts:
+        sp = storedProducts[product.id]
+        return not sp.compare(product)
+    return True
+
 def removeProduct(product: Product):
+    storedProducts.pop(product.id, None)
     redis.delete(product.id)
 
 def updateProduct(product: Product):
-    print(product.id, jsons.dumps(product))
-    redis.set(product.id, jsons.dumps(product))
+    if hasProductChanged(product):
+        storedProducts[product.id] = product
+        print(product.id, jsons.dumps(product))
+        redis.set(product.id, jsons.dumps(product))
 
 def onProductFetch(product: Product):
     with fetchLock:
-        if product.inStock > 0:
-            updateProduct(product)
-        else:
-            removeProduct(product)
+        if product.inStock > 0: updateProduct(product)
+        else: removeProduct(product)
